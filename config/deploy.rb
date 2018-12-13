87af444c08a187d12597b44becf09c5ef0aa6ca7
Dominic Bonneau <dom.bonex10@gmail.com>
2018-12-13 13:00:03 -0500

    deployment update

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,8 @@ lock "~> 3.11.0"
 
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
-set :application, "jp"
-set :repo_url, "https://github.com/dboutin84/Rocket_elevator_API.git"
+set :application, "dominic"
+set :repo_url, "https://github.com/dbone10/Rocket_Elevator_Foundation_Consolidation.git"
 # set :git_http_username, "username"
 # set :git_http_password, "password"
 
